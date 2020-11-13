cec018df6189529776c50e9293e0f1cb4fc16f3a
Charles-Chou <1774949602@qq.com>
2020-11-13 16:44:07 +0800

    分模块创建项目

---
+++ gouyan-admin/src/main/java/com/gouyan/GouyanAdminApplication.java
@@ -0,0 +1,13 @@
+package com.gouyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GouyanAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GouyanAdminApplication.class, args);
+	}
+
+}
---
+++ gouyan-common/src/main/java/com/gouyan/GouyanCommonApplication.java
@@ -0,0 +1,13 @@
+package com.gouyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GouyanCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GouyanCommonApplication.class, args);
+	}
+
+}
