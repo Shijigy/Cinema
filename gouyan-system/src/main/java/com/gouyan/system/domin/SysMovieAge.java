8f733da1bb347b308f1638fe3759b95447cbce7d
Charles-Chou <1774949602@qq.com>
2020-11-17 17:14:48 +0800

    添加自定义日志文件配置

--- gouyan-system/src/main/java/com/gouyan/system/domin/SysMovieAge.java
+++ gouyan-system/src/main/java/com/gouyan/system/domin/SysMovieAge.java
@@ -1,7 +1,6 @@
 package com.gouyan.system.domin;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 /**
@@ -15,7 +14,7 @@ public class SysMovieAge implements Serializable {
 
     private Long movieAgeId;//年代id
 
-    @NotEmpty(message = "年代名称不能为空")
+    @NotBlank(message = "年代名称不能为空")
     private String movieAgeName;//年代名称
 
     public Long getMovieAgeId() {
