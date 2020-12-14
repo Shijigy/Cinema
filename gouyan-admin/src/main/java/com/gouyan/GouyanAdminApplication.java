f45afc0d77e2104c4be6f4e3d062bef2b8c8555b
Charles-Chou <1774949602@qq.com>
2020-12-14 23:31:34 +0800

    添加anji-plus验证码，更新需要npm install 、pom重新导入

--- gouyan-admin/src/main/java/com/gouyan/GouyanAdminApplication.java
+++ gouyan-admin/src/main/java/com/gouyan/GouyanAdminApplication.java
@@ -2,7 +2,9 @@ package com.gouyan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.anji.captcha", "com.gouyan"})
 @SpringBootApplication
 public class GouyanAdminApplication {
 
