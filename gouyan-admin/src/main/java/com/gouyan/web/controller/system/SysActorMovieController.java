a86547c44e28bf84a2d4b73b68e8701d9a6d1caa
Charles-Chou <1774949602@qq.com>
2020-12-16 15:23:31 +0800

    修改数据问题

--- gouyan-admin/src/main/java/com/gouyan/web/controller/system/SysActorMovieController.java
+++ gouyan-admin/src/main/java/com/gouyan/web/controller/system/SysActorMovieController.java
@@ -6,10 +6,7 @@ import com.gouyan.system.service.impl.SysActorMovieServiceImpl;
 import com.gouyan.web.controller.BaseController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author lxd
@@ -32,7 +29,7 @@ public class SysActorMovieController extends BaseController {
         return getResult(sysActorMovieService.add(sysActorMovie));
     }
 
-    @PostMapping("/sysActorMovie/delete")
+    @DeleteMapping("/sysActorMovie")
     public ResponseResult delete(@RequestBody SysActorMovie sysActorMovie){
         return getResult(sysActorMovieService.delete(sysActorMovie));
     }
